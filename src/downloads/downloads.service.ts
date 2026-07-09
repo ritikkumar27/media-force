1c4b40c1f873e02237d70b7c18a1220a03dceb12
ritikkumar27 <rkv2771@gmail.com>
2026-07-09 16:45:57 +0530

    service : downloads

--- src/downloads/downloads.service.ts
+++ src/downloads/downloads.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
+export interface DownloadItem {
+    id: string;
+    url: string;
+    status: 'pending' | 'downloading' | 'completed' | 'failed' ;
+}
+
+
 @Injectable()
-export class DownloadsService {}
+export class DownloadsService {
+    private downloads: DownloadItem[] = [];
+
+    addDownload(url : string) {
+        const id = Date.now().toString();
+        const newDownload: DownloadItem = {
+            id,
+            url,
+            status: 'pending',
+        };
+
+        this.downloads.push(newDownload);
+        return newDownload;
+    }
+
+    getDownloads() {
+        return this.downloads;
+    }
+
+}
