a9b8d6d5da7286cee6336f59d31080383c1f9f96
ritikkumar27 <rkv2771@gmail.com>
2026-07-09 16:57:09 +0530

    controller : downloads

--- src/downloads/downloads.controller.ts
+++ src/downloads/downloads.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { DownloadsService } from './downloads.service';
 
 @Controller('downloads')
-export class DownloadsController {}
+export class DownloadsController {
+
+    constructor(private readonly downloadsService: DownloadsService){}
+
+
+    // handles POST localhost:3000/downloads
+    @Post()
+    addDownload(@Body('url') url: string){
+        if(!url){
+            return {error: 'URL is requires'};
+        }
+        return this.downloadsService.addDownload(url);
+    }
+
+    // handles GET localhost:3000/downloads
+    @Get()
+    getDownloads(){
+        return this.downloadsService.getDownloads();
+    }
+
+
+
+
+}
