10615a7b0860c770e561aafaea7e4ff462616e32
ritikkumar27 <rkv2771@gmail.com>
2026-07-09 15:34:51 +0530

    feature: downloads - first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DownloadsModule } from './downloads/downloads.module';
 
 @Module({
-  imports: [],
+  imports: [DownloadsModule],
   controllers: [AppController],
   providers: [AppService],
 })
