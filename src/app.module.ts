fa6d3c0c1f546d9b6d8d57c835c034992c80f6b5
ritikkumar27 <rkv2771@gmail.com>
2026-07-09 22:36:36 +0530

    db : config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DownloadsModule } from './downloads/downloads.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [DownloadsModule],
+  imports: [
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+      type: 'postgres',
+      host: configService.get<string>('DB_HOST'),
+      port: configService.get<number>('DB_PORT'),
+      username: configService.get<string>('POSTGRES_USER'),
+      password: configService.get<string>('POSTGRES_PASSWORD'),
+      database: configService.get<string>('POSTGRES_DB'),
+      autoLoadEntities: true,
+      synchronize: true, // remove: drop this in production
+
+      }),
+    }),
+
+    DownloadsModule],
   controllers: [AppController],
   providers: [AppService],
 })
