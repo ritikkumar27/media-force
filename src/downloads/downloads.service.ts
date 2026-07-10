bec0fc17b31fe46eb79da58c7a64566977184cee
ritikkumar27 <rkv2771@gmail.com>
2026-07-11 04:06:28 +0530

    feature: asynchronous downloads completed

--- src/downloads/downloads.service.ts
+++ src/downloads/downloads.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Download } from './entities/download.entity';
 import { Repository } from 'typeorm';
 import { YtDlpService } from './yt-dlp/yt-dlp.service';
+import { InjectQueue } from '@nestjs/bullmq';
+import { Queue } from 'bullmq';
 
 export interface DownloadItem {
   id: string;
@@ -16,18 +18,30 @@ export class DownloadsService {
     @InjectRepository(Download)
     private readonly downloadRepository: Repository<Download>,
     private ytDlpService: YtDlpService,
+
+    @InjectQueue('downloads')
+    private downloadsQueue: Queue,
   ) {}
 
   async addDownload(url: string, userId: string) {
     const metadata = await this.ytDlpService.fetchMetadata(url);
+
     const newDownload = this.downloadRepository.create({
       url,
       userId,
       title: metadata.title,
       thumbnail: metadata.thumbnail,
+      status: 'pending',
+    });
+    // here the heavy lifting is sent to Redis Queue, the worker of downloads.processor.ts will pick it automatically
+    const savedDownload = await this.downloadRepository.save(newDownload);
+
+    await this.downloadsQueue.add('process-video', {
+      url: savedDownload.url,
+      downloadId: savedDownload.id,
     });
 
-    return await this.downloadRepository.save(newDownload);
+    return savedDownload;
   }
 
   async getDownloads(userId: string) {
