0d64ef6750022b7e74e056762a751857976fbe35
ritikkumar27 <rkv2771@gmail.com>
2026-07-11 01:59:10 +0530

    feature: bullmq

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,31 +6,40 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
   imports: [
-
     ConfigModule.forRoot({
       isGlobal: true,
     }),
 
     TypeOrmModule.forRootAsync({
-
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-      type: 'postgres',
-      
-      // also i could have used process.env here but for testing and edge cases i decided not to
-      
-      host: configService.get<string>('DB_HOST'), 
-      port: configService.get<number>('DB_PORT'),
-      username: configService.get<string>('POSTGRES_USER'),
-      password: configService.get<string>('POSTGRES_PASSWORD'),
-      database: configService.get<string>('POSTGRES_DB'),
-      autoLoadEntities: true,
-      synchronize: true, // remove: drop this in production
+        type: 'postgres',
 
+        // also i could have used process.env here but for testing and edge cases i decided not to
+
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: true, // remove: drop this in production
+      }),
+    }),
+
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        connection: {
+          host: configService.get<string>('REDIS_HOST', 'localhost'),
+          port: configService.get<number>('REDIS_PORT', 6379),
+        },
       }),
     }),
 
@@ -38,7 +47,8 @@ import { AuthModule } from './auth/auth.module';
 
     UsersModule,
 
-    AuthModule],
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
