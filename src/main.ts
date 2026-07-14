460f45c4f11cf0d91a055aeb1d6f80f61f6d70ff
ritikkumar27 <rkv2771@gmail.com>
2026-07-14 23:33:43 +0530

    api : swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('MediaForce API')
+    .setDescription('The API documentation for MediaForce services')
+    .setVersion('1.0')
+    .addBearerAuth() // Adds JWT token support in the UI
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+    
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, 
