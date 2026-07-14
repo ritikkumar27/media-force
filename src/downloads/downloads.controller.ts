5267fa76df976bf115db2c016de6af30fd8ec382
ritikkumar27 <rkv2771@gmail.com>
2026-07-14 23:42:55 +0530

    api: swagger downloads controller

--- src/downloads/downloads.controller.ts
+++ src/downloads/downloads.controller.ts
@@ -2,16 +2,19 @@ import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common'
 import { DownloadsService } from './downloads.service';
 import { CreateDownloadDto } from './dto/create-download.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-
+@ApiTags('Downloads')
+@ApiBearerAuth()
 @UseGuards(AuthGuard)
 @Controller('downloads')
 export class DownloadsController {
 
     constructor(private readonly downloadsService: DownloadsService){}
 
-
+    @ApiOperation({ summary: 'Add a new video download' })
+    @ApiResponse({ status: 201, description: 'Download successfully added to queue.' })
+    @ApiResponse({ status: 401, description: 'Unauthorized (missing or invalid token).' })
     @Post()
     addDownload(@Body() createDownloadDto: CreateDownloadDto, @Request() req){
 
@@ -19,6 +22,9 @@ export class DownloadsController {
         return this.downloadsService.addDownload(createDownloadDto.url, userId);
     }
 
+    @ApiOperation({ summary: 'Get all downloads for the current user' })
+    @ApiResponse({ status: 200, description: 'Returns a list of downloads.' }) 
+    @ApiResponse({ status: 401, description: 'Unauthorized (missing or invalid token).' })
     @Get()
     getDownloads(@Request() req){
 
