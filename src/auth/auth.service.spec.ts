70306e0aa907a0b18def38e0681afb87d120af56
ritikkumar27 <rkv2771@gmail.com>
2026-07-15 19:24:18 +0530

    testing : auth service

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { UnauthorizedException } from '@nestjs/common';
 
-describe('AuthService', () => {
-  let service: AuthService;
+describe('AuthService (Unit)', () => {
+  let authService: AuthService;
+
+  const mockUsersService = {
+    findByEmail: jest.fn(), 
+  };
+
+  const mockJwtService = {
+    signAsync: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: mockUsersService },
+        { provide: JwtService, useValue: mockJwtService },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('login() should throw an UnauthorizedException if the user email is not found', async () => {
+    mockUsersService.findByEmail.mockResolvedValue(null);
+
+    await expect(
+        authService.login({ email: 'fake@email.com', password: '123' })
+    ).rejects.toThrow(UnauthorizedException);
   });
 });
