2260c610f01f8bf27d69e94ee807e8b06e2fed81
ritikkumar27 <rkv2771@gmail.com>
2026-07-24 08:25:26 +0530

    .dockerignore and a big fix for shared library

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // transpilePackages: ["@media-force/shared"],
+  transpilePackages: ["@media-force/shared"],
 };
 
 export default nextConfig;
