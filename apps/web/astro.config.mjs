b1affc77809888d518af1f037fdc1f6f1da440c5
ritikkumar27 <rkv2771@gmail.com>
2026-08-13 01:44:04 +0530

    astro dockerfile

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,11 +1,35 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
 });
+
+
+
+
+//------------------------------------------------------
+// // @ts-check
+// import { defineConfig } from 'astro/config';
+
+// import tailwindcss from '@tailwindcss/vite';
+
+// import node from '@astrojs/node';
+
+// // https://astro.build/config
+// export default defineConfig({
+//   vite: {
+//     plugins: [tailwindcss()]
+//   },
+
+//   adapter: node({
+//     mode: 'standalone'
+//   })
+// });
