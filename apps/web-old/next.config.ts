e93727232a0f0f89c076f7700aad8f70f2de214b
ritikkumar27 <rkv2771@gmail.com>
2026-08-13 01:19:48 +0530

    remove old front end completely from directory

--- apps/web-old/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: "standalone",
-  transpilePackages: ["@media-force/shared"],
-};
-
-export default nextConfig;
